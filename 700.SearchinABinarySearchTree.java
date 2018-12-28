bc7a31f26adb64e3b64a72d8cc20209be7b81eda
dieselhum@gmail.com <diesel@DieseldeMacBook-Pro.local>
2018-12-28 21:00:57 +0800

    700

---
+++ 700.SearchinABinarySearchTree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*my answer
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        
+        if (root.val == val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+*/
+
+/*iteration answer*/
+class Solution{
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val){
+            root = val<root.val? root.left:root.right;
+        }
+        return root;
+    }
+}
